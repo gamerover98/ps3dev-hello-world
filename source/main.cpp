0c68a8ab53fa667fd5552e730b75271192e9eac0
gamerover98 <gamerover98@hotmail.it>
2026-01-01 20:32:54 +0000

    Added ps3dev project files

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,58 @@
-#include "main.h"
+#include <cstdio>
+#include <cstdlib>
 
-#include <iostream>
+#include <io/pad.h>
+#include <sysutil/sysutil.h>
 
-int main() {
+#define GAME_MAX_PADS 1
 
-    std::cout << "Hello, World!";
+// Use int for running (C++98 style)
+int running = 1;
+
+// Callback function for system utilities
+extern "C" void sysutil_callback(const uint64_t status,
+                                 const uint64_t param,
+                                 void *userdata) {
+    (void) param;
+    (void) userdata;
+
+    if (status == SYSUTIL_EXIT_GAME) {
+        running = 0;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    (void) argc;
+    (void) argv;
+
+    // Register callback
+    sysUtilRegisterCallback(SYSUTIL_EVENT_SLOT0, sysutil_callback, nullptr);
+
+    // Initialize pad library
+    ioPadInit(GAME_MAX_PADS);
+
+    // [RPCS3 TTY console]
+    std::printf("Hello World from PS3!\n");
+    std::printf("Press START to exit.\n");
+
+    padInfo2 padInfo;
+    padData padData;
+
+    while (running) {
+        sysUtilCheckCallback();
+        ioPadGetInfo2(&padInfo);
+
+        for (unsigned int i = 0; i < padInfo.max; i++) {
+            if (padInfo.port_status[i]) {
+                ioPadGetData(i, &padData);
+
+                if (padData.BTN_START) {
+                    running = 0;
+                }
+            }
+        }
+    }
+
+    ioPadEnd();
     return 0;
 }
